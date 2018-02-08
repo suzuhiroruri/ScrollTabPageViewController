5b6c8da6b283992627df045a1ec5b3fdf61ff8e4
hir-suzuki <hir-suzuki@baitorunomini.local>
2018-02-08 17:37:33 +0900

    webView動作するように修正

--- ScrollTabPageViewController/Classes/BAScoutDetailAboutBenefitViewController.swift
+++ ScrollTabPageViewController/Classes/BAScoutDetailAboutBenefitViewController.swift
@@ -7,18 +7,48 @@
 //
 
 import UIKit
+import WebKit
 
-class BAScoutDetailAboutBenefitViewController: UIViewController {
+class BAScoutDetailAboutBenefitViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet var aboutScoutBenefit: UINavigationItem!
-
+    @IBOutlet var containerView: UIView!
+    var webview:WKWebView? = WKWebView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        aboutScoutBenefit.title = "スカウト特典について"
-        // Do any additional setup after loading the view.
+        aboutScoutBenefit.title = "スカウトとは"
+        
+        guard let webview:WKWebView = webview else {
+            return
+        }
+        webview.navigationDelegate = self
+        webview.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.addSubview(webview)
+        
+        // 上辺の制約
+        webview.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0.0).isActive = true
+        // 下辺の制約
+        webview.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0.0).isActive = true
+        // 左辺の制約
+        webview.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 0.0).isActive = true
+        // 右辺の制約
+        webview.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 0.0).isActive = true
+        
+        
+        // 表示するWEBサイトのURLを設定します。
+        let url = URL(string: "https://www.baitoru.com/info/scout/icon_explanation.html")
+        let urlRequest = URLRequest(url: url!)
+        // webViewで表示するWEBサイトの読み込みを開始します。
+        webview.load(urlRequest)
     }
 
     @IBAction func tapDismissButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tapReloadButton(_ sender: UIBarButtonItem) {
+        webview?.reload()
     }
 
 }
