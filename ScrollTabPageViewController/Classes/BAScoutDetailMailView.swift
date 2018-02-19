6c546cfceb1cdbb0e677c5c7e84b620b21901692
hir-suzuki <hir-suzuki@baitorunomini.local>
2018-02-19 18:08:45 +0900

    xib入れる

--- ScrollTabPageViewController/Classes/BAScoutDetailMailView.swift
+++ ScrollTabPageViewController/Classes/BAScoutDetailMailView.swift
@@ -71,7 +71,7 @@ class BAScoutDetailMailView: UIView {
         scrollView.delegate = self
         benefitCollectionView.delegate = self
         benefitCollectionView.dataSource = self
-        mailHeaderLabel.text = "彼らはほかことに同じ経験人についてののうちをすれでで。いったい今を学習心は幾分その講演たますなどに投げ出しばいなとは修養しませないが、少しにはすれたんたませ。主義で聴かた事はもとより今日がけっしてたんませ。けっして大森さんの話重き少々尊敬の云いない詩この二つ私か発展をという皆発表だたでたいから、漠然たるたくさんは私か否一団を立たから、張さんの事に金力のそれにもうご附着と使えるから私人がご研究へ云っようにもちろんご努力をあろますでて、とうとうよほど講演に進まですてならですのにできなあっ。"
+        mailHeaderLabel.text = "彼らはほかことに同じ経験人についてののうちを"
         mailHeaderLabel.sizeToFit()
 
         guard let benefitRemarksIsEmpty = scoutDetailMailViewModel?.benefitRemarks?.isEmpty else {
